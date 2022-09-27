fc60c092554d3c5e21c8598750b502226b2b84c1
Tot3sMalot3s <bkatsevych@icloud.com>
2022-09-27 21:46:14 +0300

    display test

--- src/components/StockList.jsx
+++ src/components/StockList.jsx
@@ -2,13 +2,12 @@ import React, { useEffect, useState } from "react";
 import finnHub from "../api/finnHub";
 
 const StockList = () => {
-    const [stock, setStock] = useState();
+    const [stock, setStock] = useState([]);
     const [watchList, setWatchList] = useState(["GOOGL", "MSFT", "AMZN"]);
 
     useEffect(() => {
         let isMounted = true;
         const fetchData = async () => {
-            const responses = [];
             try {
                 const responses = await Promise.all(
                     watchList.map((stock) => {
@@ -19,10 +18,19 @@ const StockList = () => {
                         });
                     })
                 );
-                console.log(responses);
+
+                const data = responses.map((response) => {
+                    return {
+                        data: response.data,
+                        symbol: response.config.params.symbol,
+                    };
+                });
+
                 if (isMounted) {
-                    setStock(responses);
+                    setStock(data);
                 }
+
+                console.log(stock);
             } catch (e) {}
         };
         fetchData();
@@ -30,7 +38,40 @@ const StockList = () => {
         return () => (isMounted = false);
     }, []);
 
-    return <div>I am a stocklist component</div>;
+    return (
+        <>
+            <table className="table hover mt-5">
+                <thead style={{ color: "rgb(79, 89, 102)" }}>
+                    <tr>
+                        <th scope="col">Name</th>
+                        <th scope="col">Last</th>
+                        <th scope="col">Chg</th>
+                        <th scope="col">Chg%</th>
+                        <th scope="col">High</th>
+                        <th scope="col">Low</th>
+                        <th scope="col">Open</th>
+                        <th scope="col">Pclose</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {stock.map((stockData) => {
+                        return (
+                            <tr className="table-row" key={stockData.symbol}>
+                                <th scope="row">{stockData.symbol}</th>
+                                <td>{stockData.data.c}</td>
+                                <td>{stockData.data.d}</td>
+                                <td>{stockData.data.dp}</td>
+                                <td>{stockData.data.h}</td>
+                                <td>{stockData.data.l}</td>
+                                <td>{stockData.data.o}</td>
+                                <td>{stockData.data.pc}</td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </>
+    );
 };
 
 export default StockList;

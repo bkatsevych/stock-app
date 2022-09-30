61a060883d387b7b81bf34865a7befcf6e606870
Tot3sMalot3s <bkatsevych@icloud.com>
2022-09-30 16:42:59 +0300

    individ data formatted

--- src/pages/StockDetailPage.jsx
+++ src/pages/StockDetailPage.jsx
@@ -1,19 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import finnHub from "../api/finnHub";
+import StockChart from "../components/StockChart";
+
+const formatData = (data) => {
+    return data.t.map((el, index) => {
+        return {
+            x: el * 1000,
+            y: Math.floor(data.c[index]),
+        };
+    });
+};
 
 const StockDetailPage = () => {
+    const [chartData, setChartData] = useState();
     const { symbol } = useParams();
-
     useEffect(() => {
         const fetchData = async () => {
             const date = new Date();
             const currentTime = Math.floor(date.getTime() / 1000);
             let oneDay;
-
-            if (date.getDate === 6) {
+            if (date.getDay() === 6) {
                 oneDay = currentTime - 2 * 24 * 60 * 60;
-            } else if (date.getDate === 0) {
+            } else if (date.getDay() === 0) {
                 oneDay = currentTime - 3 * 24 * 60 * 60;
             } else {
                 oneDay = currentTime - 24 * 60 * 60;
@@ -49,14 +58,28 @@ const StockDetailPage = () => {
                     }),
                 ]);
                 console.log(responses);
-            } catch (e) {
-                console.log(e);
+
+                setChartData({
+                    day: formatData(responses[0].data),
+                    week: formatData(responses[1].data),
+                    year: formatData(responses[2].data),
+                });
+            } catch (err) {
+                console.log(err);
             }
         };
         fetchData();
-    }, []);
+    }, [symbol]);
 
-    return <div>I am a StockDetailPage {symbol} </div>;
+    return (
+        <>
+            {chartData && (
+                <div>
+                    <StockChart chartData={chartData} symbol={symbol} />
+                </div>
+            )}
+        </>
+    );
 };
 
 export default StockDetailPage;

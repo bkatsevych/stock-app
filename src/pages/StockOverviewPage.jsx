38ae35bbd91623d3ce4f4a7a306b68282b6e9063
Tot3sMalot3s <bkatsevych@icloud.com>
2022-09-30 23:32:54 +0300

    localstorage

--- src/pages/StockOverviewPage.jsx
+++ src/pages/StockOverviewPage.jsx
@@ -5,7 +5,6 @@ import StockList from "../components/StockList";
 const StockOverviewPage = () => {
     return (
         <div>
-            I am the StockOverviewPage
             <Autocomplete />
             <StockList />
         </div>
